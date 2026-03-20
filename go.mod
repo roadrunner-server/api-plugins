ac5b79d1637ab6784ba5f08c090e052ca88c58c5
Valery Piashchynski <piashchynski.valery@gmail.com>
2026-03-20 12:20:05 +0100

    chore: update logger interface
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- go.mod
+++ go.mod
@@ -1,7 +1,3 @@
 module github.com/roadrunner-server/api-plugins/v6
 
 go 1.26
-
-require go.uber.org/zap v1.27.1
-
-require go.uber.org/multierr v1.11.0 // indirect
--- logger/doc.go
+++ logger/doc.go
@@ -1,5 +1,4 @@
 // Package logger defines logging interfaces for RoadRunner plugins.
-// The Log interface wraps go.uber.org/zap, exposing leveled methods from Debug
-// through Fatal. The Named interface provides NamedLogger for obtaining a
-// logger scoped to a specific plugin or component name.
+// The Named interface provides NamedLogger for obtaining a [log/slog.Logger]
+// scoped to a specific plugin or component name.
 package logger
